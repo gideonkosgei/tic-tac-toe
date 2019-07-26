ec9200709e916ac36baa42f75929dfcdf6e3313e
Gideon Kosgei <mr.gkosgei@gmail.com>
2019-07-26 14:44:11 +0300

    finalized game construction

--- src/components/board.js
+++ src/components/board.js
@@ -1,22 +1,35 @@
 import React from 'react';
 import './../App.css';
 import Square from './square';
+import calculateWinner from './calculateWinner';
 
 class Board extends React.Component {
-    constructor() {
-      super();
-      this.state = {
-        squares: Array(9).fill(null),
-      };
+    
+
+  handleClick(i) {
+    const history = this.state.history;
+    const current = history[history.length - 1];
+    const squares = current.squares.slice();
+    if (calculateWinner(squares) || squares[i]) {
+      return;
     }
+    squares[i] = this.state.xIsNext ? 'X' : 'O';
+    this.setState({
+      history: history.concat([{
+        squares: squares
+      }]),
+      xIsNext: !this.state.xIsNext,
+    });
+  }
+
     renderSquare(i) {
-      return <Square value={this.state.squares[i]} />;
+      return <Square value={this.props.squares[i]} onClick={() => this.props.onClick(i)} />;
     }
     render() {
-      const status = 'Next player: X';
+      
       return (
         <div>
-          <div className="status">{status}</div>
+          
           <div className="board-row">
             {this.renderSquare(0)}
             {this.renderSquare(1)}
